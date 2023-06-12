121dd1241098ecce3ff61d66f55c18d8f1a2e36b
Frederico Pellegrini <fredericopellegrini@proton.me>
2023-06-11 23:51:37 -0300

    Atualização com a Tela Inicial

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     event.preventDefault(); // Evita que o formulário seja enviado
 
     // Redirecionar para a página inicial após o login
-    window.location.href = "pagina-inicial.html";
+    window.location.href = "TelaInicial/telaInicial.html";
 });
