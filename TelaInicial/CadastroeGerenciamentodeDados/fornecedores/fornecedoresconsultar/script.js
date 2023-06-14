954a80bfe54a470bd172f9df2ab8545c8769c00e
FredericoPellegrini <fredericopellegrini@proton.me>
2023-06-14 08:37:05 -0300

    0.8

--- TelaInicial/CadastroeGerenciamentodeDados/fornecedores/fornecedoresconsultar/script.js
+++ TelaInicial/CadastroeGerenciamentodeDados/fornecedores/fornecedoresconsultar/script.js
@@ -1,8 +1,25 @@
 // Botão "Voltar" - Retorna para a página anterior
 document.querySelector('.back-btn').addEventListener('click', function() {
-    history.back();
-  });
+  history.back();
+});
+
+document.querySelector('.logout-btn').addEventListener('click', function() {
+  window.location.href = '../../../../index.html';
+});
+
+// Função de busca
+document.querySelector('.search-btn').addEventListener('click', function() {
+  var searchInput = document.querySelector('.search-input').value;
+  var tableRows = document.querySelectorAll('.product-table tbody tr');
   
-  document.querySelector('.logout-btn').addEventListener('click', function() {
-    window.location.href = '../../../../index.html';
+  tableRows.forEach(function(row) {
+    var codeCell = row.querySelector('td:first-child');
+    var code = codeCell.textContent;
+    
+    if (code === searchInput) {
+      row.style.display = 'table-row';
+    } else {
+      row.style.display = 'none';
+    }
+  });
 });
