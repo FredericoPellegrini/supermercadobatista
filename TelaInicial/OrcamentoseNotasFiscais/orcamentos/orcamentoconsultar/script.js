cedf1efd37a7c7640338b9b09774f9f0f00f028d
FredericoPellegrini <fredericopellegrini@proton.me>
2023-06-14 16:01:16 -0300

    3.0

---
+++ TelaInicial/OrcamentoseNotasFiscais/orcamentos/orcamentoconsultar/script.js
@@ -0,0 +1,25 @@
+// Botão "Voltar" - Retorna para a página anterior
+document.querySelector('.back-btn').addEventListener('click', function() {
+    history.back();
+  });
+  
+  document.querySelector('.logout-btn').addEventListener('click', function() {
+    window.location.href = '../../../../index.html';
+});
+
+// Função de busca
+document.querySelector('.search-btn').addEventListener('click', function() {
+  var searchInput = document.querySelector('.search-input').value;
+  var tableRows = document.querySelectorAll('.product-table tbody tr');
+  
+  tableRows.forEach(function(row) {
+    var codeCell = row.querySelector('td:first-child');
+    var code = codeCell.textContent;
+    
+    if (code === searchInput) {
+      row.style.display = 'table-row';
+    } else {
+      row.style.display = 'none';
+    }
+  });
+});
---
+++ TelaInicial/OrcamentoseNotasFiscais/script.js
@@ -0,0 +1,14 @@
+// Botão Voltar
+var backButton = document.querySelector('.back-btn');
+backButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  history.back(); // Volta para a página anterior no histórico do navegador
+});
+
+// Botão Sair
+var logoutButton = document.querySelector('.logout-btn');
+logoutButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  window.location.href = '../../index.html';
+});
+
