72854ba078a377e2e1e152a29098156a6011d6d2
FredericoPellegrini <fredericopellegrini@proton.me>
2023-06-14 10:28:31 -0300

    0.9

--- TelaInicial/CadastroeGerenciamentodeDados/transportadoras/transportadorasconsultar/script.js
+++ TelaInicial/CadastroeGerenciamentodeDados/transportadoras/transportadorasconsultar/script.js
@@ -6,3 +6,20 @@ document.querySelector('.back-btn').addEventListener('click', function() {
   document.querySelector('.logout-btn').addEventListener('click', function() {
     window.location.href = '../../../../index.html';
 });
+
+// Função de busca
+document.querySelector('.search-btn').addEventListener('click', function() {
+  var searchInput = document.querySelector('.search-input').value;
+  var tableRows = document.querySelectorAll('.product-table tbody tr');
+  
+  tableRows.forEach(function(row) {
+    var codeCell = row.querySelector('td:first-child');
+    var code = codeCell.textContent;
+    
+    if (code === searchInput) {
+      row.style.display = 'table-row';
+    } else {
+      row.style.display = 'none';
+    }
+  });
+});
