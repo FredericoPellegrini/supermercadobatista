ef4f9148f1bd36361f33b1cd128be09cfa783d57
FredericoPellegrini <fredericopellegrini@proton.me>
2023-06-14 00:24:45 -0300

    0.5 - Produtos

--- TelaInicial/CadastroeGerenciamentodeDados/script.js
+++ TelaInicial/CadastroeGerenciamentodeDados/script.js
@@ -1,12 +1,26 @@
-// Script.js
-
 // Botão "Voltar" - Retorna para a página anterior
 document.querySelector('.back-btn').addEventListener('click', function() {
-    history.back();
+  history.back();
+});
+
+// Botão "Logout" - Redireciona para a página index.html duas pastas acima
+document.querySelector('.logout-btn').addEventListener('click', function() {
+  window.location.href = '../../index.html';
+});
+
+// Links de cadastro de produtos
+var cadastrarLinks = document.querySelectorAll('.produtocadastrar');
+cadastrarLinks.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'produtos/produtosCadastrar/index.html';
   });
-  
-  // Botão "Logout" - Redireciona para a página index.html duas pastas acima
-  document.querySelector('.logout-btn').addEventListener('click', function() {
-    window.location.href = '../../index.html';
+});
+
+var cadastrarLinks = document.querySelectorAll('.produtocadastrar');
+cadastrarLinks.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'produtos/produtosConsultar/index.html';
   });
-  
+});
