ef6356fb3fbbb62515d0f6c4e282c15cc0354c53
FredericoPellegrini <fredericopellegrini@proton.me>
2023-06-14 10:41:26 -0300

    1.0

--- TelaInicial/CadastroeGerenciamentodeDados/script.js
+++ TelaInicial/CadastroeGerenciamentodeDados/script.js
@@ -1,165 +1,168 @@
-// Botão "Voltar" - Retorna para a página anterior
-document.querySelector('.back-btn').addEventListener('click', function() {
-  history.back();
+// Botão Voltar
+var backButton = document.querySelector('.back-btn');
+backButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  history.back(); // Volta para a página anterior no histórico do navegador
 });
 
-// Botão "Logout" - Redireciona para a página index.html duas pastas acima
-document.querySelector('.logout-btn').addEventListener('click', function() {
+// Botão Sair
+var logoutButton = document.querySelector('.logout-btn');
+logoutButton.addEventListener('click', function(event) {
+  event.preventDefault();
   window.location.href = '../../index.html';
 });
 
+
+
 // Links de cadastro de produtos
-var cadastrarLinks = document.querySelectorAll('.produtocadastrar');
-cadastrarLinks.forEach(function(link) {
+var cadastrarProdutosLinks = document.querySelectorAll('.produtocadastrar');
+cadastrarProdutosLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'produtos/produtosCadastrar/index.html';
   });
 });
 
-var cadastrarLinks = document.querySelectorAll('.produtoconsultar');
-cadastrarLinks.forEach(function(link) {
+var consultarProdutosLinks = document.querySelectorAll('.produtoconsultar');
+consultarProdutosLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'produtos/produtosConsultar/index.html';
   });
 });
 
-//Links de cadastro de clientes
-var cadastrarLinks = document.querySelectorAll('.clientescadastrar');
-cadastrarLinks.forEach(function(link) {
+// Links de cadastro de clientes
+var cadastrarClientesLinks = document.querySelectorAll('.clientescadastrar');
+cadastrarClientesLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'clientes/clientescadastrar/index.html';
   });
 });
 
-var cadastrarLinks = document.querySelectorAll('.clientesconsultar');
-cadastrarLinks.forEach(function(link) {
+var consultarClientesLinks = document.querySelectorAll('.clientesconsultar');
+consultarClientesLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'clientes/clientesconsultar/index.html';
   });
 });
 
-//Links de cadastro de clientes
-var cadastrarLinks = document.querySelectorAll('.operacoesfiscaiscadastrar');
-cadastrarLinks.forEach(function(link) {
+// Links de cadastro de operações fiscais
+var cadastrarOperacoesLinks = document.querySelectorAll('.operacoesfiscaiscadastrar');
+cadastrarOperacoesLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'operacoesfiscais/operacoesfiscaiscadastro/index.html';
   });
 });
 
-var cadastrarLinks = document.querySelectorAll('.operacoesfiscaisconsultar');
-cadastrarLinks.forEach(function(link) {
+var consultarOperacoesLinks = document.querySelectorAll('.operacoesfiscaisconsultar');
+consultarOperacoesLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'operacoesfiscais/operacoesfiscaisconsulta/index.html';
   });
 });
 
-======================================================================================
-
-// Empresas
-var cadastrarLinks = document.querySelectorAll('empresascadastrar');
-cadastrarLinks.forEach(function(link) {
+// Links de cadastro de empresas
+var cadastrarEmpresasLinks = document.querySelectorAll('.empresascadastrar');
+cadastrarEmpresasLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'empresas/empresascadastrar/index.html';
   });
 });
 
-var cadastrarLinks = document.querySelectorAll('.empresasconsultar');
-cadastrarLinks.forEach(function(link) {
+var consultarEmpresasLinks = document.querySelectorAll('.empresasconsultar');
+consultarEmpresasLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'empresas/empresasconsultar/index.html';
   });
 });
 
-//Fornecedores
-var cadastrarLinks = document.querySelectorAll('.fornecedorescadastrar');
-cadastrarLinks.forEach(function(link) {
+// Links de cadastro de fornecedores
+var cadastrarFornecedoresLinks = document.querySelectorAll('.fornecedorescadastrar');
+cadastrarFornecedoresLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'fornecedores/fornecedorescadastrar/index.html';
   });
 });
 
-var cadastrarLinks = document.querySelectorAll('.fornecedoresconsultar');
-cadastrarLinks.forEach(function(link) {
+var consultarFornecedoresLinks = document.querySelectorAll('.fornecedoresconsultar');
+consultarFornecedoresLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'fornecedores/fornecedoresconsultar/index.html';
   });
 });
 
-//Plano de contas
-var cadastrarLinks = document.querySelectorAll('.planodecontascadastrar');
-cadastrarLinks.forEach(function(link) {
+// Links de cadastro de plano de contas
+var cadastrarPlanoContasLinks = document.querySelectorAll('.planodecontascadastrar');
+cadastrarPlanoContasLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
-    window.location.href = 'planodecontas/planodecontascadastro/index.html';
+    window.location.href = 'planodecontas/planodecontascadastrar/index.html';
   });
 });
 
-var cadastrarLinks = document.querySelectorAll('.planodecontasconsultar');
-cadastrarLinks.forEach(function(link) {
+var consultarPlanoContasLinks = document.querySelectorAll('.planodecontasconsultar');
+consultarPlanoContasLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
-    window.location.href = 'planodecontas/planodecontasconsulta/index.html';
+    window.location.href = 'planodecontas/planodecontasconsultar/index.html';
   });
 });
 
-================================================================================================================+
-// Serviços
-var cadastrarLinks = document.querySelectorAll('servicoscadastrar');
-cadastrarLinks.forEach(function(link) {
+// Links de cadastro de serviços
+var cadastrarServicosLinks = document.querySelectorAll('.servicoscadastrar');
+cadastrarServicosLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'servicos/servicoscadastrar/index.html';
   });
 });
 
-var cadastrarLinks = document.querySelectorAll('servicosconsultar');
-cadastrarLinks.forEach(function(link) {
+var consultarServicosLinks = document.querySelectorAll('.servicosconsultar');
+consultarServicosLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'servicos/servicosconsultar/index.html';
   });
 });
 
-//Funcionarios
-var cadastrarLinks = document.querySelectorAll('.funcionarioscadastrar');
-cadastrarLinks.forEach(function(link) {
+// Links de cadastro de funcionários
+var cadastrarFuncionariosLinks = document.querySelectorAll('.funcionarioscadastrar');
+cadastrarFuncionariosLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'funcionarios/funcionarioscadastrar/index.html';
   });
 });
 
-var cadastrarLinks = document.querySelectorAll('.funcionariosconsultar');
-cadastrarLinks.forEach(function(link) {
+var consultarFuncionariosLinks = document.querySelectorAll('.funcionariosconsultar');
+consultarFuncionariosLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'funcionarios/funcionariosconsultar/index.html';
   });
 });
 
-//Transportadora
-var cadastrarLinks = document.querySelectorAll('.transportadoracadastrar');
-cadastrarLinks.forEach(function(link) {
+// Links de cadastro de transportadoras
+var cadastrarTransportadorasLinks = document.querySelectorAll('.transportadorascadastrar');
+cadastrarTransportadorasLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
-    window.location.href = 'transportadora/transportadoracadastro/index.html';
+    window.location.href = 'transportadoras/transportadorascadastrar/index.html';
   });
 });
 
-var cadastrarLinks = document.querySelectorAll('.transportadoraconsultar');
-cadastrarLinks.forEach(function(link) {
+var consultarTransportadorasLinks = document.querySelectorAll('.transportadorasconsultar');
+consultarTransportadorasLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
-    window.location.href = 'transportadora/transportadoraconsulta/index.html';
+    window.location.href = 'transportadoras/transportadorasconsultar/index.html';
   });
 });
